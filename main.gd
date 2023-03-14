db0a08e9981c73f9fd53510e3e00de95fd1821a6
Doug Noël <dougnoel@gmail.com>
2023-03-14 18:21:36 -0400

    Randomized cards and added max hand size.

--- main.gd
+++ main.gd
@@ -1,9 +1,11 @@
 extends Control
 
-@export var card_front: PackedScene = preload("res://Cards/giant_growth_card.tscn")
+@export var card_front_1: PackedScene = preload("res://Cards/giant_growth_card.tscn")
+@export var card_front_2: PackedScene = preload("res://Cards/blanchwood_armor_card.tscn")
 
 var deck: Array
 var hand = 0
+var max_hand_size = 5
 
 func _ready():
 	deck = [1,2,1,2,1,2,1,2,1]
@@ -20,10 +22,22 @@ func _drop_data(pos, data):
 
 # Returns a random card from the deck
 func _draw_card():
-	pass
+	hand += 1
+	var my_card = deck.pick_random()
+	
+	match my_card:
+		1:
+			return card_front_1
+		2:
+			return card_front_2
 	
 
+func _draw_five_cards():
+	while hand < max_hand_size:
+		var instance = _draw_card().instantiate()
+		$"Player Hand".add_child(instance)
+		await get_tree().create_timer(0.3).timeout
+		
 func _on_deck_draw_card():
-	var instance = card_front.instantiate()
-	$"Player Hand".add_child(instance)
+	_draw_five_cards()
 
